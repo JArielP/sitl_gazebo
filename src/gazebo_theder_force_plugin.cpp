436899aa3594f4b659fa4db4224e6d91f543153a
Jonas Peter <peterjo@student.ethz.ch>
2017-12-21 15:28:58 +0100

    new tether force constants

--- src/gazebo_theder_force_plugin.cpp
+++ src/gazebo_theder_force_plugin.cpp
@@ -19,8 +19,8 @@ ThederForcePlugin::ThederForcePlugin()
 {
   this->i=0;
   this->ropeLength = 150;
-  this->forceConstantA = 6;
-  this->forceConstantB = 5;
+  this->forceConstantA = 20.0f*0.5f*9.81f/(expf(0.2f*this->forceConstantB));
+  this->forceConstantB = 121000.0f/(20.0f*0.5f*9.81f);
   this->dragConst = 0.002347995;
   this->dampingConstant = 3;
   this->eModule = 121000;
